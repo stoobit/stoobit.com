b76110a0f10f6587fa1c08c4d702a90c194cd612
Till <till.bruegmann@icloud.com>
2025-08-04 00:56:35 +0200

    scholarship preperations

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -11,6 +11,7 @@ struct Home: StaticPage {
         VStack(alignment: .leading, spacing: 20) {
             ContactCard
             VitalityPro
+            StoobitAnalytics
             AllProjects
         }
         .frame(width: .percent(100%))
@@ -95,14 +96,14 @@ struct Home: StaticPage {
         VStack(spacing: 25) {
             Image(
                 "/icons/swift.svg",
-                description: "The Swift Logo."
+                description: "The Swift logo."
             )
             .resizable()
             .frame(width: .px(100), height: .px(100))
             
             VStack(spacing: 5) {
                 Text("Vitality Pro")
-                    .font(.title1)
+                    .font(.title2)
                     .fontWeight(.bold)
                 
                 Text("Swift Student Challenge Winner")
@@ -140,6 +141,87 @@ struct Home: StaticPage {
                 .style(.padding, "10px 24px")
                 .style(.backgroundColor, "#00000000")
                 .border(Color(hex: "#FF9230"), width: 2)
+                .style(.color, "black")
+                .style(.textDecoration, "none")
+                .style(.borderRadius, "9999px")
+                .style(.fontWeight, "600")
+            }
+        }
+        .style(.textAlign, "center")
+        .frame(height: .percent(100%))
+        .padding(.horizontal, 20)
+    }
+    
+    // stoobit analytics
+    var StoobitAnalytics: some HTML {
+        VStack {
+            StoobitAnalyticsText
+                .class("col-md-6 order-1 order-md-1 pb-5 pb-md-0")
+            
+            StoobitAnalyticsImage
+                .class("col-md-6 order-2 order-md-2")
+        }
+        .class("row g-0 flex-column flex-md-row align-items-center")
+        .padding(50)
+        .frame(width: .percent(100%))
+        .background(Color(hex: "#F5F5F8"))
+    }
+    var StoobitAnalyticsImage: some HTML {
+        Image(
+            "/images/vitality_pro_preview.jpeg",
+            description: "Two screenshots of Vitality Pro."
+        )
+        .resizable()
+        .width(5)
+    }
+    var StoobitAnalyticsText: some HTML {
+        VStack(spacing: 25) {
+            Image(
+                "/icons/analytics.svg",
+                description: "The stoobit analytics logo."
+            )
+            .resizable()
+            .frame(width: .px(100), height: .px(100))
+            
+            VStack(spacing: 5) {
+                Text("stoobit analytics")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text("Check24 Scholarship Project")
+                    .font(.title4)
+                    .fontWeight(.regular)
+                    .foregroundStyle(Color.gray)
+            }
+           
+            VStack(spacing: 10) {
+                let buttonWidth: Int = 250
+                
+                let learnmore = "https://apps.apple.com/us/app/vitality-pro/id6478023736"
+                let check24 = "https://www.talents.check24.de/"
+                
+                Link(target: learnmore) {
+                    Span("Learn More")
+                }
+                .border(Color.bootstrapBlue, width: 2)
+                .frame(width: buttonWidth)
+                .style(.display, "inline-block")
+                .style(.padding, "10px 24px")
+                .background(Color.bootstrapBlue)
+                .style(.color, "white")
+                .style(.textDecoration, "none")
+                .style(.borderRadius, "9999px")
+                .style(.fontWeight, "600")
+                
+                Link(target: check24) {
+                    Span("About the Scholarship")
+                }
+                .frame(width: buttonWidth)
+                .target(.blank)
+                .style(.display, "inline-block")
+                .style(.padding, "10px 24px")
+                .style(.backgroundColor, "#00000000")
+                .border(Color.bootstrapBlue, width: 2)
                 .style(.color, "black")
                 .style(.textDecoration, "none")
                 .style(.borderRadius, "9999px")
